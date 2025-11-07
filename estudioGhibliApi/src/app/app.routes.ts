bfceab9edbae03ad097ba26478d357beda6f4f30
nayaaruiz <nayaaaruiz@gmail.com>
2025-11-07 13:45:38 +0100

    deatils

--- estudioGhibliApi/src/app/app.routes.ts
+++ estudioGhibliApi/src/app/app.routes.ts
@@ -1,9 +1,16 @@
 import { Routes } from '@angular/router';
 import { CharacterPage } from './pages/character-page/character-page';
+import { CharacterDetailsComponent } from './components/character-details-component/character-details-component';
+import { CharacterDetailsPage } from './pages/character-details-page/character-details-page';
 
 export const routes: Routes = [
     {
         path: 'characters',
         component: CharacterPage
+    },
+
+    {
+        path: 'character/:id',
+        component: CharacterDetailsPage
     }
 ];
